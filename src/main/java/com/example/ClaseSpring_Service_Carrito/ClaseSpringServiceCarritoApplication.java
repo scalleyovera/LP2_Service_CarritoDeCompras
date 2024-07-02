56edaa1e1e99aa7000104a7104ce0cff13f45517
Danielito games <scalleyovera@gmail.com>
2024-07-02 10:46:12 -0500

    creacion de proyecto en spring

---
+++ src/main/java/com/example/ClaseSpring_Service_Carrito/ClaseSpringServiceCarritoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClaseSpring_Service_Carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaseSpringServiceCarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaseSpringServiceCarritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ClaseSpring_Service_Carrito/ClaseSpringServiceCarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClaseSpring_Service_Carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaseSpringServiceCarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
